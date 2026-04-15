c6488da6e4385f32d682eeb5e65f0b346339075d
CodeLavkush <mauryalavkush19@gmail.com>
2026-04-15 22:50:10 +0530

    Project setup with healthcheck route

---
+++ backend/src/app.js
@@ -0,0 +1,30 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express()
+
+// basic configurations
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+// cors configurations
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["content-type", "authorization"],
+}))
+
+// import the routes
+import healthCheckRouter from "./routes/healthcheck.routes.js"
+
+app.use("/api/v1/healthcheck", healthCheckRouter)
+
+app.get('/', (req, res) => {
+    res.send("Welcome to inventory management system backend!")
+})
+
+export default app
