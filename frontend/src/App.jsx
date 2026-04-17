2f10e30ff0efec891859b46e82be41dbe2435c60
CodeLavkush <mauryalavkush19@gmail.com>
2026-04-17 22:32:54 +0530

    Frontend project setup with tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-white bg-gray-900 w-screen h-screen flex justify-center items-center text-4xl'>App</div>
+  )
+}
+
+export default App
